4df4f1b3deee81aa17c79dcf3b1c6160a331b5d4
splint314 <krigal323@gmail.com>
2026-05-18 14:57:47 +0200

    feat: made a script who create locked boxes and unlock it or still get them locked

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def canUnlockAll(boxes):
+
+    if not boxes:
+        return False
+
+    unlocked = [0]
+    for box_id in unlocked:
+        for key in boxes[box_id]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.append(key)
+
+    return len(unlocked) == len(boxes)
